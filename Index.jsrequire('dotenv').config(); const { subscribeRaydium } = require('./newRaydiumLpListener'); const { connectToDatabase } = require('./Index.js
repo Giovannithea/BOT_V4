5c0567aa6ce2dcf314f71672640b68065cb39389
Giovannithea <joseseb200@gmail.com>
2024-10-13 21:22:12 -0400

    Delete Index.jsrequire('dotenv').config(); const { subscribeRaydium } = require('./newRaydiumLpListener'); const { connectToDatabase } = require('. directory

--- Index.jsrequire('dotenv').config(); const { subscribeRaydium } = require('./newRaydiumLpListener'); const { connectToDatabase } = require('./Index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { subscribeRaydium } = require('./newRaydiumLpListener');
-const { connectToDatabase } = require('./newRaydiumLpService');
-
-(async () => {
-    await connectToDatabase();
-    subscribeRaydium().catch(console.error);
-})();
